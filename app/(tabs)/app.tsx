a56ea088a1cd99958992fa991973aac6a6fb3237
AbrahamMora <mora78606@gmail.com>
2024-07-17 11:56:50 -0700

    Splash Screen

---
+++ app/(tabs)/app.tsx
@@ -0,0 +1,16 @@
+// App.tsx
+// App.tsx
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  
+};
+
+export default App;
